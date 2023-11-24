3fa399ebf74d33223cfecdab8408463b0dc0c420
Harry Buxton <hxb273@student.bham.ac.uk>
2023-11-24 21:18:01 +0000

    removed json returns

--- src/actions/searchSong.tsx
+++ src/actions/searchSong.tsx
@@ -11,27 +11,4 @@ export async function searchSong(formData: FormData) {
   const redirectUrl = songName.replace(/ /g, "-");
 
   redirect(`/search/${redirectUrl}`);
-
-  /*
-    const url = `https://genius-song-lyrics1.p.rapidapi.com/search/?q=${songName}&per_page=10&page=1`
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': `${process.env.API_KEY}`,
-        'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
-      }
-    };
-
-    try {
-      const response = await fetch(url, options);
-      if (response.ok){
-        const redirectUrl = songName.replace(/ /g, '-');
-        redirect(`/search/${"redirectUrl"}`);
-      } else {
-        throw new Error(`HTTP Error: ${response.status}`);
-      }
-    } catch (error) {
-        console.log(error)
-    }
-    */
 }
